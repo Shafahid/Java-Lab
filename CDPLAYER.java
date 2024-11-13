ffb1e89f31be81d77a29ac91d5ba8e4f92b83ccd
H. M. Mehedi Hasan <141162383+Mehedi26696@users.noreply.github.com>
2024-11-13 22:51:08 +0600

    file

--- CDPLAYER.java
+++ CDPLAYER.java
@@ -1,3 +1,7 @@
+
+//Name: H.M.Mehedi Hasan
+//Roll: FH-13
+
 import java.util.Scanner;
 
 public class CDPLAYER {
@@ -38,70 +42,70 @@ public class CDPLAYER {
             return;
         }
 
-        System.out.println("Enter 1 to play the song or 2 to get the information about the song: \n");
+        System.out.println("Enter Choices: \n");
 
-        System.out.println("1. Play the song\n2. Get information about the song \n");
+        System.out.println("1. Play songs\n2. Get information about songs \n");
 
         int choice = sc.nextInt();
-
-        if(choice == 1){
+        
+        if (choice == 1) {
             System.out.println("Enter the number of the song you want to play: \n");
             int n = sc.nextInt();
 
             if (str.equals("Audio")) {
                 if (n == 1) {
-                    if(a.songs[0].getTitle().contains(".mp3")){
-                        a.mp3player(a.songs[0].getTitle(), a.songs[0]);
-                    } else if(a.songs[0].getTitle().contains(".wav")){
-                        a.wavplayer(a.songs[0].getTitle(), a.songs[0]);
-                    } else if(a.songs[0].getTitle().contains(".AAC")){
-                        a.AACplayer(a.songs[0].getTitle(), a.songs[0]);
+                    if (a.songs[0].getTitle().contains(".mp3")) {
+                        a.mp3player(str, a.songs[0]);
+                    } else if (a.songs[0].getTitle().contains(".wav")) {
+                        a.wavplayer(str, a.songs[0]);
+                    } else if (a.songs[0].getTitle().contains(".AAC")) {
+                        a.AACplayer(str, a.songs[0]);
                     }
                 } else if (n == 2) {
-                    if(a.songs[1].getTitle().contains(".mp3")){
-                        a.mp3player(a.songs[1].getTitle(), a.songs[1]);
-                    } else if(a.songs[1].getTitle().contains(".wav")){
-                        a.wavplayer(a.songs[1].getTitle(), a.songs[1]);
-                    } else if(a.songs[1].getTitle().contains(".AAC")){
-                        a.AACplayer(a.songs[1].getTitle(), a.songs[1]);
+                    if (a.songs[1].getTitle().contains(".mp3")) {
+                        a.mp3player(str, a.songs[1]);
+                    } else if (a.songs[1].getTitle().contains(".wav")) {
+                        a.wavplayer(str, a.songs[1]);
+                    } else if (a.songs[1].getTitle().contains(".AAC")) {
+                        a.AACplayer(str, a.songs[1]);
                     }
                 } else if (n == 3) {
-                    if(a.songs[2].getTitle().contains(".mp3")){
-                        a.mp3player(a.songs[2].getTitle(), a.songs[2]);
-                    } else if(a.songs[2].getTitle().contains(".wav")){
-                        a.wavplayer(a.songs[2].getTitle(), a.songs[2]);
-                    } else if(a.songs[2].getTitle().contains(".AAC")){
-                        a.AACplayer(a.songs[2].getTitle(), a.songs[2]);
+                    if (a.songs[2].getTitle().contains(".mp3")) {
+                        a.mp3player(str, a.songs[2]);
+                    } else if (a.songs[2].getTitle().contains(".wav")) {
+                        a.wavplayer(str, a.songs[2]);
+                    } else if (a.songs[2].getTitle().contains(".AAC")) {
+                        a.AACplayer(str, a.songs[2]);
                     }
-                } 
+                }
 
                 else {
                     System.out.println("Invalid choice\n");
                 }
-            } else if(str.equals("Video")) {
+            } else if (str.equals("Video")) {
                 if (n == 1) {
-                    if(v.songs[0].getTitle().contains(".mp4")){
-                        v.mp4player(v.songs[0].getTitle(), v.songs[0]);
-                    } else if(v.songs[0].getTitle().contains(".avi")){
-                        v.aviplayer(v.songs[0].getTitle(), v.songs[0]);
-                    } else if(v.songs[0].getTitle().contains(".mkv")){
-                        v.mkvplayer(v.songs[0].getTitle(), v.songs[0]);
+                    if (v.songs[0].getTitle().contains(".mp4")) {
+                        v.mp4player(str, v.songs[0]);
+                    } else if (v.songs[0].getTitle().contains(".avi")) {
+                        v.aviplayer(str, v.songs[0]);
+                    } else if (v.songs[0].getTitle().contains(".mkv")) {
+                        v.mkvplayer(str, v.songs[0]);
                     }
                 } else if (n == 2) {
-                    if(v.songs[1].getTitle().contains(".mp4")){
-                        v.mp4player(v.songs[1].getTitle(), v.songs[1]);
-                    } else if(v.songs[1].getTitle().contains(".avi")){
-                        v.aviplayer(v.songs[1].getTitle(), v.songs[1]);
-                    } else if(v.songs[1].getTitle().contains(".mkv")){
-                        v.mkvplayer(v.songs[1].getTitle(), v.songs[1]);
+                    if (v.songs[1].getTitle().contains(".mp4")) {
+                        v.mp4player(str, v.songs[1]);
+                    } else if (v.songs[1].getTitle().contains(".avi")) {
+                        v.aviplayer(str, v.songs[1]);
+                    } else if (v.songs[1].getTitle().contains(".mkv")) {
+                        v.mkvplayer(str, v.songs[1]);
                     }
                 } else if (n == 3) {
-                    if(v.songs[2].getTitle().contains(".mp4")){
-                        v.mp4player(v.songs[2].getTitle(), v.songs[2]);
-                    } else if(v.songs[2].getTitle().contains(".avi")){
-                        v.aviplayer(v.songs[2].getTitle(), v.songs[2]);
-                    } else if(v.songs[2].getTitle().contains(".mkv")){
-                        v.mkvplayer(v.songs[2].getTitle(), v.songs[2]);
+                    if (v.songs[2].getTitle().contains(".mp4")) {
+                        v.mp4player(str, v.songs[2]);
+                    } else if (v.songs[2].getTitle().contains(".avi")) {
+                        v.aviplayer(str, v.songs[2]);
+                    } else if (v.songs[2].getTitle().contains(".mkv")) {
+                        v.mkvplayer(str, v.songs[2]);
                     }
                 }
 
@@ -109,7 +113,7 @@ public class CDPLAYER {
                     System.out.println("Invalid choice\n");
                 }
             }
-        } else if(choice == 2) {
+        } else if (choice == 2) {
             System.out.println("Enter the number of the song you want to get information about: \n");
             int n = sc.nextInt();
 
@@ -121,7 +125,7 @@ public class CDPLAYER {
                 } else if (n == 3) {
                     a.displayInformation(a.songs[2]);
                 }
-            } else if(str.equals("Video")) {
+            } else if (str.equals("Video")) {
                 if (n == 1) {
                     v.displayInformation(v.songs[0]);
                 } else if (n == 2) {
@@ -180,7 +184,7 @@ class Song {
 class Audio extends Player {
 
     Song[] songs = {
-            
+
             new Song("Billie Jean.wav", "Michael Jackson", "1983-05-05"),
             new Song("Wavin' Flag.mp3", "K'naan", "2009-03-18"),
             new Song("Hotel California.AAC", "Eagles", "1977-06-06")
@@ -229,20 +233,23 @@ class Player extends Screen {
     }
 }
 
-
-class Screen{
+class Screen {
     void play(String file, String type, Song song) {
         System.out.println("Playing " + file + " song " + song.getTitle() + " using " + type);
         song.setPlaying(true);
     }
 
     void displayInfoPrompt(Song song) {
-        System.out.println("To know more about the song, enter 1: \n");
+        System.out.println("Are you want to know more about the song? \n");
+        System.out.println("1. Yes\n2. No\n");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         if (n == 1) {
             displayInformation(song);
+        }
+        else {
+            System.out.println("Invalid choice\n");
         }
 
         sc.close();
@@ -252,12 +259,11 @@ class Screen{
         System.out.println("Title: " + song.getTitle());
         System.out.println("Artist: " + song.getArtist());
         System.out.println("Release Date: " + song.getReleaseDate());
-        
-        if(song.isPlaying()){
+
+        if (song.isPlaying()) {
             System.out.println("The song is currently playing\n");
         } else {
             System.out.println("The song is not currently playing\n");
         }
     }
 }
-
